f57e4841da86a6ead6f15d078047d339f37d6f02
RomaYD <74683772+RomaYD@users.noreply.github.com>
2025-02-04 20:41:02 +0300

    Update vk-bot-test.py

--- vk-bot-test.py
+++ vk-bot-test.py
@@ -40,7 +40,15 @@ def main():
             for event in response['updates']:
                 if event['type'] == 'message_allow':
                     user_id = event['object']['user_id']
-                    send_message(user_id, 'Привет! Отправь мне изображение, и я пришлю его обратно. :)')
+                    message_history = requests.get(API_URL + 'messages.getHistory', params={
+                        'group_id': GROUP_ID,
+                        'access_token': ACCESS_TOKEN,
+                        'v': API_VERSION,
+                        'user_id': user_id,
+                        'start_message_id': 0
+                    }).json()
+                    if message_history['response']['count'] == 0:
+                        send_message(user_id, 'Привет! Отправь мне изображение, и я пришлю его обратно. :)')
                 if event['type'] == 'message_new':
                     user_id = event['object']['message']['from_id']
                     attachments = event['object']['message'].get('attachments', [])
